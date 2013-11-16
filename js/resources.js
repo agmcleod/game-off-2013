2e113b3884058bfc71bd12fea1c926f4d48b2a78
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-11-15 19:50:36 -0500

    replaced imagery and map file

--- js/resources.js
+++ js/resources.js
@@ -1,5 +1,5 @@
 game.resources = [
-  { name : 'simple-bg', type : 'image', src : 'data/img/simple-bg.png' },
+  { name : 'grass', type : 'image', src : 'data/img/grass.png' },
   { name : 'simple', type : 'tmx', src : 'data/tmx/simple.tmx' },
   { name : 'player', type : 'image', src : 'data/img/playersketch.png' }
 ];

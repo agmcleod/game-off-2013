ec24bca4a41519b37b5b72a13a75e7b679ccdaca
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-12-27 12:27:26 -0500

    updated melon

--- js/play_controller.js
+++ js/play_controller.js
@@ -2,19 +2,12 @@ game.PlayController = Object.extend({
   init : function() {
     me.entityPool.add('player', game.Player);
     me.levelDirector.loadLevel('simple');
-    this.player = me.game.getEntityByName('player')[0];
+    this.player = me.game.world.getEntityByName('player')[0];
     me.game.currentLevel.getLayerByName('Parallax_level1').repeat = 'no-repeat';
     this.collidable = true;
   },
 
   update : function() {
-    this.updateMovement();
-    if(this.vel.x !== 0 || this.vel.y !== 0) {
-      this.parent();
-      return true;
-    }
-    else {
-      return false;
-    }
+
   }
 })
